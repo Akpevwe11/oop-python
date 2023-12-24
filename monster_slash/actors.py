1b8e39cb5a48acf2b9836b0564d10e602351b9a8
Akpevwe <essiakpevwepraise@gmail.com>
2023-12-24 15:40:38 +0100

    enemy game

---
+++ monster_slash/actors.py
@@ -0,0 +1,38 @@
+#!/bin/bash/python3
+
+from random import randint
+
+class Player:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+    
+    def __repr__(self):
+        return ('<Player: {} at level{}>'
+                .format(self.name, 
+                self.level))
+
+    def get_attack_power(self):
+        return randint(1, 100) * self.level
+
+
+class Enemy: 
+    def __init__(self, kind, level):
+        self.kind = kind
+        self.level = level
+
+    def __repr__(self):
+        return '<Enemy: {}>'.format(self.kind)
+
+    def get_attack_power(self):
+        return randint(1, 100) * self.level
+
+
+if __name__ == '__main__':
+    player = Player(name='Hercules', level=1)
+    print(player)
+    enemy = Enemy(kind='Ogre', level=1)
+    print(enemy)
+    print(enemy.level)
+    print(enemy.get_attack_power())
+
